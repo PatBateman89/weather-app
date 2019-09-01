92b9033367bd9535b5146eb7b2f40f7d1048292a
PatBateman89 <feric89@gmail.com>
2019-09-01 16:52:48 +0900

    styles added

--- public/js/app.js
+++ public/js/app.js
@@ -13,18 +13,23 @@ fetch('http://localhost:3000/weather?address=!').then((response) => {
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
   const location = search.value
 
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
+
   fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
-        console.log(data)
+        messageOne.textContent = data.error
       } else {
-        console.log(data.location)
-        console.log(data.forecast)
+        messageOne.textContent = ''
+        messageTwo.textContent = `${data.location}: ${data.forecast}`
       }
     })
   })
