be7059d791ecbc9a4e8eb8ecd6838396c74a74af
PatBateman89 <feric89@gmail.com>
2019-09-01 17:20:40 +0900

    changes made for heroku deployment

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,5 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data)
-    } else {
-      console.log(data.location)
-      console.log(data.forecast)
-    }
-  })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -23,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = 'Loading...'
   messageTwo.textContent = ''
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+  fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error
